d467eb03845fbcc5cd848c8f4bfe6d02223f2228
Anyieth Aguek Ayuen <77279583+Aguek@users.noreply.github.com>
2022-11-26 23:54:17 +0300

    Update main.py
    Added a condition to check whether the user typed the state more than once, if he answered that state then he shouldn't be awarded for it for the second time.

--- u.s_states_quiz/main.py
+++ u.s_states_quiz/main.py
@@ -17,20 +17,22 @@ state_file = pandas.read_csv("50_states.csv")
 states = state_file.state.to_list()
 # check if the guess is among the 50 states
 score = 0
-
+correct_guesses = []
 while score < 50:
     screen.title(f"{score}/{50} States")
     answer_state = screen.textinput(title="Guess", prompt="What's another state name?").title()
     if answer_state in states:
-        x_val = state_file[state_file.state == answer_state].x
-        y_val = state_file[state_file.state == answer_state].y
-        state_turtle = turtle.Turtle()
-        state_turtle.hideturtle()
-        state_turtle.penup()
-        state_turtle.goto(float(x_val), float(y_val))
-        state_turtle.write(answer_state)
+        if answer_state not in correct_guesses:
+            x_val = state_file[state_file.state == answer_state].x
+            y_val = state_file[state_file.state == answer_state].y
+            state_turtle = turtle.Turtle()
+            state_turtle.hideturtle()
+            state_turtle.penup()
+            state_turtle.goto(float(x_val), float(y_val))
+            state_turtle.write(answer_state)
 
-        score += 1
+            score += 1
+        correct_guesses.append(answer_state)
        
 
 turtle.mainloop()
